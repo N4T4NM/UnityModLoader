4b26a3a6840f00c0ed7b4cfc4f441b9f0a9f0934
NatanM <natanaelmarian@hotmail.com>
2021-10-15 10:22:53 -0300

    Added mod sample

---
+++ ModSample.AutoLoader/LoaderUI.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityModLoader.Library.Core;
+
+namespace ModSample.AutoLoader
+{
+    public class LoaderUI : MonoBehaviour
+    {
+        public List<FileInfo> Files = new List<FileInfo>();
+
+        Rect windowRect = new Rect(0, 0, 500, 70);
+        Rect nButton = new Rect(5, 20, 100, 45);
+        Rect yButton = new Rect(500 - 105, 20, 100, 45);
+        private void OnGUI()
+        {
+            if (Files.Count == 0)
+                return;
+
+            GUI.Window(0, windowRect, ExecWindow, $"Do you want to load \"{Files[0].Name}\"");
+        }
+
+        void ExecWindow(int id)
+        {
+            if (GUI.Button(nButton, "No"))
+            {
+                UnityModLoader.Library.Core.Logging.Logger.Instance.Log($"Mod \"{Files[0].Name}\" ignored by user.");
+
+                Files[0].MoveTo(Path.Combine(Files[0].Directory.FullName, $"{Files[0].Name}.disabled"));
+                Files.RemoveAt(0);
+                return;
+            }
+
+            if (GUI.Button(yButton, "Yes"))
+            {
+
+                try
+                {
+                    ModLoader.Instance.LoadModAssembly(Files[0].FullName);
+                    UnityModLoader.Library.Core.Logging.Logger.Instance.Log($"Mod \"{Files[0].Name}\" loaded by user.");
+                }
+                catch (Exception ex)
+                {
+                    UnityModLoader.Library.Core.Logging.Logger.Instance.Log($"Could not load \"{Files[0].Name}\". {ex.Message}", messageType: UnityModLoader.Library.Core.Logging.Logger.MessageType.Warning);
+                    Files[0].MoveTo(Path.Combine(Files[0].Directory.FullName, $"{Files[0].Name}.disabled"));
+                }
+
+                Files.RemoveAt(0);
+                return;
+            }
+        }
+    }
+}
---
+++ ModSample.AutoLoader/ModAutoLoader.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using UnityModLoader.Library.Core.Logging;
+using UnityModLoader.Library.Mods;
+using UnityModLoader.Library.Mods.Attributes;
+
+namespace ModSample.AutoLoader
+{
+    [MainClass]
+    public class ModAutoLoader
+    {
+        [EntryPoint]
+        public static void Run()
+        {
+            UnityEngine.GameObject holder = new UnityEngine.GameObject("ModLoaderUI");
+            UnityEngine.GameObject.DontDestroyOnLoad(holder);
+            LoaderUI ui = holder.AddComponent<LoaderUI>();
+
+            FileSystemWatcher watcher = new FileSystemWatcher("./Mods", "*.dll");
+            watcher.NotifyFilter = NotifyFilters.FileName | NotifyFilters.DirectoryName;
+            watcher.Created += (s, e) =>
+                {
+                    Logger.Instance.Log($"New mod file detected, {e.Name}");
+                    ui.Files.Add(new FileInfo(e.FullPath));
+                };
+            watcher.EnableRaisingEvents = true;
+        }
+    }
+}
