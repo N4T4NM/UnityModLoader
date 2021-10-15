fa3501836422b11f9f8bdc1e2301da5a712d1d21
NatanM <natanaelmarian@hotmail.com>
2021-10-15 13:14:35 -0300

    Added support for Name, Description and Author

--- UnityModLoader.Library/Mods/Attributes/MainClassAttribute.cs
+++ UnityModLoader.Library/Mods/Attributes/MainClassAttribute.cs
@@ -5,6 +5,8 @@ namespace UnityModLoader.Library.Mods.Attributes
     [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
     public class MainClassAttribute : Attribute
     {
-
+        public string ModName { get; set; }
+        public string ModDescription { get; set; }
+        public string ModAuthor { get; set; }
     }
 }
