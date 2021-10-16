862cc3ee841ed6f204e54ee6e0aaaf1b53082fa5
NatanM <natanaelmarian@hotmail.com>
2021-10-16 16:28:32 -0300

    Fixed namespace

--- UnityModLoader.Library/Mods/Attributes/EntryPointAttribute.cs
+++ UnityModLoader.Library/Mods/Attributes/EntryPointAttribute.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace UnityModLoader.Library.Mods
+namespace UnityModLoader.Library.Mods.Attributes
 {
     [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
     public class EntryPointAttribute : Attribute { }
