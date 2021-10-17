21b1aa2889f1d6f648d122a67595ecdbab7ff569
NatanM <natanaelmarian@hotmail.com>
2021-10-17 15:01:52 -0300

    Remove unused classes

--- UnityModLoader.Library/Core/Exceptions/InvalidClassException.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace UnityModLoader.Library.Core.Exceptions
-{
-    public class InvalidClassException : Exception
-    {
-        public InvalidClassException(Type mainClass) : base($"Main Class: {mainClass.Name}\n\"EntryPointAttribute\" not found.") { }
-    }
-}
--- UnityModLoader.Library/Mods/Attributes/EntryPointAttribute.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace UnityModLoader.Library.Mods.Attributes
-{
-    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
-    public class EntryPointAttribute : Attribute { }
-    
-}
