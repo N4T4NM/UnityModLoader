cca4e41d478a9d0fc3438c68301a70997489a9d9
NatanM <natanaelmarian@hotmail.com>
2021-10-17 15:02:05 -0300

    Minor change

--- UnityModLoader.Library/Core/Exceptions/InvalidAssemblyException.cs
+++ UnityModLoader.Library/Core/Exceptions/InvalidAssemblyException.cs
@@ -5,6 +5,6 @@ namespace UnityModLoader.Library.Core.Exceptions
 {
     public class InvalidAssemblyException : Exception
     {
-        public InvalidAssemblyException(Assembly asm) : base($"Assembly: {asm.GetName().Name}\n\"MainClassAttribute\" not found.") { }
+        public InvalidAssemblyException(Assembly asm) : base($"Assembly: {asm.GetName().Name}\nNot a valid unity mod.") { }
     }
 }
